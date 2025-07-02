7d8bee9d8e6b45ac57e6944eed2018dfcae50208
Nabisudeen <nabisudeen2004@gmail.com>
2025-07-02 17:04:49 +0530

    commit

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'nabisudeen2004@gmail.com',
+    phone: '+91 6383984009',
+    address: '',
+    github: 'https://github.com/Nabisudeen',
+    facebook: '',
+    linkedIn: 'https://linkedin.com/in/nabisudeen-h',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: ""
 }
